e2bf8e409f9d6a3beac7117bb988123835c1737f
sowdm <matthew.sowd@gmail.com>
2023-08-04 20:21:14 -0400

    Initial demo version of dashboard for team buy-in

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import logging
+import os
 import math
 import pandas as pd
 import openpolicedata as opd
@@ -9,15 +10,80 @@ import streamlit as st
 default_logger = logging.getLogger("critical")
 default_logger.setLevel(logging.CRITICAL)
 
-def get_data():
+def get_population():
+    df = load_csv('FairfaxCountyPopulation.csv',index_col="Race/Ethnicity")
+    return df["Population"]
+
+def get_data(source_name="Virginia", table_type="STOPS", agency="Fairfax County Police Department"):
     logger = logging.getLogger("opd-app")
+
+    tables = ["result", "search", "search_na", "person_search_na", "vehicle_search_na",'uof_officer','uof_subject']
+    data = {}
+    for t in tables:
+        csv_filename = f"{source_name}_{agency}_{table_type}_{t}.csv"
+        data[t] = load_csv(csv_filename)
+        data[t]["Month"] = pd.to_datetime(data[t]["Month"], errors="coerce").dt.to_period("M")
+        
+    start_date = max([x["Month"].max() for x in data.values()])
+    start_date = str(start_date)+"-01"
+
+    # Try to update with most recent data
+    try:
+        new_data = download_data(source_name, table_type, agency, start_date=start_date, logger=logger)
+
+        for t in tables:
+            new_months = new_data[t]["Month"].unique()
+            # For months in both datasets, replace with new data
+            data[t] = data[t][~data[t]['Month'].isin(new_months)]
+            data[t] = pd.concat([data[t], new_data[t]], ignore_index=True)
+    except:
+        pass
+
+    return data
+
+def get_scorecard(data, population, reason_for_stop, period):
+    if period == "ALL":
+        months = data['result']["Month"].unique()
+        df = data['result']
+        # df_search = data['search']
+        df_search_na = data['search_na']
+    elif period == "MOST RECENT YEAR":
+        most_recent_month = data['result']["Month"].max()
+        months = data['result']["Month"][data['result']["Month"]>most_recent_month-12].unique()
+        df = data['result'][data['result']["Month"]>most_recent_month-12]
+        # df_search = data['search'][data['search']["Month"]>most_recent_month-12]
+        df_search_na = data['search_na'][data['search_na']["Month"]>most_recent_month-12]
+    else:
+        months = data['result']["Month"][data['result']["Month"].dt.year==data].unique()
+        df = data['result'][data['result']["Month"].dt.year==period]
+        # df_search = data['search'][data['search']["Month"].dt.year==period]
+        df_search_na = data['search_na'][data['search_na']["Month"].dt.year==period]
+
+    is_arrest = df["action_taken"]=="ARREST"
+    result = {}
+    if reason_for_stop == "ALL":
+        result['Total Stops'] = df.groupby("Race/Ethnicity").sum(numeric_only=True).sum(axis=1).convert_dtypes()
+        total_arrests = df[is_arrest].groupby("Race/Ethnicity").sum(numeric_only=True).sum(axis=1).convert_dtypes()
+        # total_searches = df_search.groupby("Race/Ethnicity").sum(numeric_only=True).sum(axis=1).convert_dtypes()
+        total_searches_na = df_search_na.groupby("Race/Ethnicity").sum(numeric_only=True).sum(axis=1).convert_dtypes()
+    else:
+        result['Total Stops'] = df.groupby("Race/Ethnicity")[reason_for_stop].sum()
+        total_arrests = df[is_arrest].groupby("Race/Ethnicity")[reason_for_stop].sum()
+        # total_searches = df_search.groupby("Race/Ethnicity")[reason_for_stop].sum()
+        total_searches_na = df_search_na.groupby("Race/Ethnicity")[reason_for_stop].sum()
+
+    result['Stops per 1000 People'] = (result['Total Stops'] / population * 1000)[result['Total Stops'].index]
+    result['Arrest Rate'] = total_arrests / result['Total Stops'] * 100
+    # result['Search Rate'] = total_searches / result['Total Stops'] * 100
+    result['Search Rate (Non-Arrests Only)*'] = total_searches_na / (result['Total Stops']) * 100
+
+    return pd.DataFrame(result)
+
 
 def download_data(source_name, table_type, agency, start_date="2020-01-01", logger=default_logger):
 
     cur_year = datetime.now().year
     stop_date = f"{cur_year+1}-01-01"
-
-    # TODO: Update start_date based on saved data
 
     src = opd.Source(source_name)
     record_count = src.get_count([start_date, stop_date], table_type, agency=agency)
@@ -41,9 +107,9 @@ def download_data(source_name, table_type, agency, start_date="2020-01-01", logg
         df["Month"] = df["incident_date"].dt.to_period("M")
         df["Quarter"] = df["incident_date"].dt.to_period("Q")
         df["Race/Ethnicity"] = df["race"].replace({
-            "BLACK OR AFRICAN AMERICAN":"Black",
+            "BLACK OR AFRICAN AMERICAN":"BLACK",
             'ASIAN OR NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER':"AAPI",
-            'AMERICAN INDIAN OR ALASKA NATIVE':"Indigenous"
+            'AMERICAN INDIAN OR ALASKA NATIVE':"Indigenous".upper()
         }).str.upper()
 
         eth = [x for x in df["ethnicity"].unique() if "HISPANIC" in x and not x.upper().startswith("NOT")]
@@ -62,16 +128,41 @@ def download_data(source_name, table_type, agency, start_date="2020-01-01", logg
         data["person_search_na"] = df[(df["person_searched"]=="YES") & no_arrest].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
         data["vehicle_search_na"] = df[(df["vehicle_searched"]=="YES") & no_arrest].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
 
+        data["uof_officer"] = df[df["physical_force_by_officer"]=="YES"].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
+        data["uof_subject"] = df[df["physical_force_by_subject"]=="YES"].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
+
+        for k,v in data.items():
+            data[k] = v.reset_index()
+
     return data
+
+
+def load_csv(csv_filename, *args, **kwargs):
+    try:
+        return pd.read_csv("https://raw.githubusercontent.com/Fairfax-County-NAACP-Data/fcpd-data/main/data/"+
+                            csv_filename.replace(" ","%20"), *args, **kwargs)
+    except Exception as e:
+        if os.path.exists(os.path.join(r"..\fcpd-data\data", csv_filename)):
+            try:
+                print(f"LOADING CSV {csv_filename} FROM LOCAL FILE!!!")
+                return pd.read_csv(os.path.join(r"..\fcpd-data\data", csv_filename), *args, **kwargs)
+            except:
+                raise e
+        else:
+            raise e
 
 
 def update_saved_data(source_name, table_type, agency, start_date="2020-01-01", logger=default_logger):
     data = download_data(source_name, table_type, agency, start_date, logger)
     for k,v in data.items():
-        v.to_csv(rf"./data/{source_name}_{agency}_{table_type}_{k}.csv")
+        v.to_csv(rf"../fcpd-data/data/{source_name}_{agency}_{table_type}_{k}.csv")
 
 
 if __name__ == "__main__":
     import streamlit_debug as stdb
     stdb.add_debug(st)
-    update_saved_data("Virginia", "STOPS", "Fairfax County Police Department")
+    data = get_data()
+    population = get_population()
+    get_scorecard(data, population, "ALL", "ALL")
+    a = 1
+    # update_saved_data("Virginia", "STOPS", "Fairfax County Police Department")
--- fcpd_dashboard.py
+++ fcpd_dashboard.py
@@ -1,14 +1,15 @@
 import streamlit as st
 
+import data
 from datetime import datetime
-import math
 import pandas as pd
 
 from streamlit_logger import create_logger
 import openpolicedata as opd
 
-debug_mode = True
+debug_mode = False
 if debug_mode:
+    print('RUNNING IN DEBUG MODE')
     # Overwrite some streamlit function with non-functioning versions to enable Python debug
     import streamlit_debug as stdb
     stdb.add_debug(st)
@@ -18,6 +19,7 @@ st.set_page_config(
     page_title="FCPD Dashboard",
     page_icon="🌃",
     initial_sidebar_state="expanded",
+    layout = 'wide',
     menu_items={
         'Report a Bug': "https://github.com/openpolicedata/OPD_Explorer/issues"
     }
@@ -34,61 +36,145 @@ drop = ["Indigenous"]
 
 @st.cache_data(show_spinner=False)
 def get_data():
-    start_date = "2020-01-01"
-    cur_year = datetime.now().year
-    stop_date = f"{cur_year+1}-01-01"
+    return data.get_data()
 
-    # TODO: Update start_date based on saved data
+@st.cache_data(show_spinner=False)
+def get_population():
+    return data.get_population()
 
-    src = opd.Source("Virginia")
-    record_count = src.get_count([start_date, stop_date], table_type, agency=agency)
+police_data = get_data()
+population = get_population()
+
+reasons_for_stop = police_data['result'].columns.drop(["Month","Race/Ethnicity","action_taken"]).tolist()
+reasons_for_stop.insert(0, "ALL")
+
+time_periods = ["MOST RECENT YEAR", "ALL"]
+time_periods.extend([x for x in range(police_data['result']["Month"].dt.year.max(), 2019, -1)])
+
+col1, col2, col3 = st.columns(3)
+with col1:
+    selected_time = st.selectbox("Time Period", time_periods,
+                                    help="Filter to show only statistics for selected time period")
+with col2:
+    selected_reason = st.selectbox("Reason For Stop", reasons_for_stop,
+                                    # default=reasons_for_stop,
+                                    help="Filter to show only statistics for selected reason(s) for stop")
+with col3:
+    selected_races = st.multiselect("Race/Ethnicity", police_data['result']["Race/Ethnicity"].unique(),
+                                    default=["AAPI", "BLACK", "LATINO", "WHITE"],
+                                    help="Select races/ethnicities to show in data below")
     
-    df = []
-    batch_size = 5000
-    nbatches = math.ceil(record_count / batch_size)
-    iter = 0
-    wait_text = "Loading most recent data ({} of " + f"{nbatches})"
-    pbar = st.progress(0, text=wait_text.format(0))
-    for tbl in src.load_from_url_gen(year=[start_date, stop_date], table_type=table_type, nbatch=batch_size, agency=agency):
-        iter+=1
-        df.append(tbl.table)
-        pbar.progress(iter / nbatches, text=wait_text.format(iter))
-        
-    if len(df)>0:
-        df = pd.concat(df)
-        logger.debug(f"Table size is {len(df)}")
+# Determine time range
+if selected_time == "ALL":
+    months = police_data['result']["Month"].unique()
+elif selected_time == "MOST RECENT YEAR":
+    most_recent_month = police_data['result']["Month"].max()
+    months = police_data['result']["Month"][police_data['result']["Month"]>most_recent_month-12].unique()
+else:
+    months = police_data['result']["Month"][police_data['result']["Month"].dt.year==selected_time].unique()
 
-        df["Month"] = df["incident_date"].dt.to_period("M")
-        df["Quarter"] = df["incident_date"].dt.to_period("Q")
-        df["Race/Ethnicity"] = df["race"].replace({
-            "BLACK OR AFRICAN AMERICAN":"Black",
-            'ASIAN OR NATIVE HAWAIIAN OR OTHER PACIFIC ISLANDER':"AAPI",
-            'AMERICAN INDIAN OR ALASKA NATIVE':"Indigenous"
-        }).str.upper()
+st.header(f"Scorecard for {months.min().strftime('%B %Y')} to {months.max().strftime('%B %Y')}")
 
-        eth = [x for x in df["ethnicity"].unique() if "HISPANIC" in x and not x.upper().startswith("NOT")]
-        if len(eth)!=1:
-            raise ValueError(f"Ethnicity not found in {df['ethnicity'].unique() }")
-        df.loc[df["ethnicity"] == eth[0], "Race/Ethnicity"] = "LATINO"
-        df.loc[df["ethnicity"] == "UNKNOWN", "Race/Ethnicity"] = "UNKNOWN"
+scard = data.get_scorecard(police_data, population, selected_reason, selected_time)
 
-        df = df[~df["Race/Ethnicity"].isin(drop)]
+column_config={
+        "Total Stops": st.column_config.ProgressColumn(
+            format="%d",
+            min_value=0,
+            max_value=int(scard['Total Stops'].max()),
+        ),
+        "Stops per 1000 People": st.column_config.ProgressColumn(
+            format="%0.1f",
+            min_value=0,
+            max_value=float(scard['Stops per 1000 People'].max()),
+        )
+    }
 
-        action_taken_counts = df.value_counts(["Month","Race/Ethnicity","action_taken","reason_for_stop"]).unstack(fill_value=0)
+for k in range(2, len(scard.columns)):
+    column_config[scard.columns[k]] = st.column_config.ProgressColumn(
+            min_value=0,
+            max_value=float(scard[scard.columns[k]].max()),
+            format="%0.1f%%"
+        )
 
-        was_searched = (df["person_searched"]=="YES") | (df["vehicle_searched"]=="YES")
-        search_counts = df[was_searched].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
+st.dataframe(
+    scard.loc[selected_races],
+    column_config=column_config
+)
 
-        no_arrest = df["action_taken"]!="ARREST"
-        search_counts_no_arrest = df[was_searched & no_arrest].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
-        person_search_counts_no_arrest = df[(df["person_searched"]=="YES") & no_arrest].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
-        vehicle_search_counts_no_arrest = df[(df["vehicle_searched"]=="YES") & no_arrest].value_counts(["Month","Race/Ethnicity","reason_for_stop"]).unstack(fill_value=0)
+st.caption("\* In Virginia, individuals are frequently searched during an arrest (i.e. they are searched because they are arrested rather than a search leads to an arrest). "+
+        "The data provides no ability to distinguish between searches due to an arrest and discretionary searches and searches due to an arrest are the majority of searches.  "+
+        "To focus on discretionary searches, the search rate shown is only for searches that do not end in arrest. Thus, the search rate measures how often officers "+
+        "use their discretionary powers to search individuals but are unable to find evidence that leads to an arrest.")
 
-        action_taken_counts.to_csv(rf"./data/{agency}_{table_type}_actions_taken.csv")
-        search_counts.to_csv(rf"./data/{agency}_{table_type}_searches.csv")
+st.warning("Discuss denominator of search rate")
+
+if len(selected_races)>0:
+    disparity_thresh = 1.2
+    most_stops = scard.loc[selected_races]["Total Stops"].idxmax()
+
+    msg = f'{most_stops} individuals were stopped the most'
+    if selected_reason != "ALL":
+        msg += f" for {selected_reason}"
+    msg+=f': {int(scard.loc[most_stops]["Total Stops"])} stops'
+    st.info(msg)
+
+    highest_rate = scard.loc[selected_races]["Stops per 1000 People"].idxmax()
+    msg = f'{highest_rate} individuals were stopped at a rate of {scard.loc[highest_rate]["Stops per 1000 People"]:.1f} stops'
+    if selected_reason != "ALL":
+        msg += f" for {selected_reason}"
+    msg+=f' per 1000 people (relative to Fairfax Co. population).'
+    if highest_rate!="WHITE":
+        st.info(msg+f' This is {scard.loc[highest_rate]["Stops per 1000 People"]/scard.loc["WHITE"]["Stops per 1000 People"]:.1f} '+
+                    'times higher than the rate for WHITE individuals.')
+        for x in selected_races:
+            if x!=highest_rate and scard.loc[x]["Stops per 1000 People"]/scard.loc["WHITE"]["Stops per 1000 People"]>disparity_thresh:
+                msg = f'{x} individuals were stopped at a rate of {scard.loc[x]["Stops per 1000 People"]:.1f} stops'
+                if selected_reason != "ALL":
+                    msg += f" for {selected_reason}"
+                msg+=f' per 1000 people (relative to Fairfax Co. population).'
+                st.info(msg+f' This is {scard.loc[x]["Stops per 1000 People"]/scard.loc["WHITE"]["Stops per 1000 People"]:.1f} '+
+                    'times higher than the rate for WHITE individuals.')
     else:
-        logger.debug("No new data found")
+        st.info(msg)
 
-get_data()
+    highest_rate = scard.loc[selected_races]["Arrest Rate"].idxmax()
+    msg = f'{highest_rate} individuals are arrested in {scard.loc[highest_rate]["Arrest Rate"]:.1f}% of stops'
+    if selected_reason != "ALL":
+        msg += f" for a {selected_reason}"
+    if highest_rate!="WHITE":
+        st.info(msg+f'. This is {scard.loc[highest_rate]["Arrest Rate"]/scard.loc["WHITE"]["Arrest Rate"]:.1f} '+
+                    'times higher than the rate for WHITE individuals.')
+        for x in selected_races:
+            if x!=highest_rate and scard.loc[x]["Arrest Rate"]/scard.loc["WHITE"]["Arrest Rate"]>disparity_thresh:
+                msg = f'{x} individuals are arrested in {scard.loc[x]["Arrest Rate"]:.1f}% of stops'
+                if selected_reason != "ALL":
+                    msg += f" for {selected_reason}"
+                st.info(msg+f'. This is {scard.loc[x]["Arrest Rate"]/scard.loc["WHITE"]["Arrest Rate"]:.1f} '+
+                    'times higher than the rate for WHITE individuals.')
+    else:
+        st.info(msg)
+
+    highest_rate = scard.loc[selected_races]["Search Rate (Non-Arrests Only)*"].idxmax()
+    msg = f'{highest_rate} individuals are searched in {scard.loc[highest_rate]["Search Rate (Non-Arrests Only)*"]:.1f}% of stops that do not end in arrest'
+    if selected_reason != "ALL":
+        msg += f" for {selected_reason}"
+    if highest_rate!="WHITE":
+        st.info(msg+f'. This is {scard.loc[highest_rate]["Search Rate (Non-Arrests Only)*"]/scard.loc["WHITE"]["Search Rate (Non-Arrests Only)*"]:.1f} '+
+                    'times higher than the rate for WHITE individuals.')
+        for x in selected_races:
+            if x!=highest_rate and scard.loc[x]["Search Rate (Non-Arrests Only)*"]/scard.loc["WHITE"]["Search Rate (Non-Arrests Only)*"]>disparity_thresh:
+                msg = f'{x} individuals are searched in {scard.loc[x]["Search Rate (Non-Arrests Only)*"]:.1f}% of stops that do not end in arrest'
+                if selected_reason != "ALL":
+                    msg += f" for {selected_reason}"
+                st.info(msg+f'. This is {scard.loc[x]["Search Rate (Non-Arrests Only)*"]/scard.loc["WHITE"]["Search Rate (Non-Arrests Only)*"]:.1f} '+
+                    'times higher than the rate for WHITE individuals.')
+    else:
+        st.info(msg)
+    
+
+st.warning("TODO: Stops per 1000 people needs to be calculated on annual basis")
+st.warning("TODO: Confirm that statistics are correct")
+st.warning("Adding info about search rate no arrest")
 
 logger.debug(f'Done with rendering dataframe using OPD Version {opd.__version__}')
